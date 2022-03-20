af9fbd75398585c59fa5fa07ff9fd2808a55080f
Aryan <shivangisinghhappy@gmail.com>
2022-03-20 15:03:06 +0530

    Jquery Css

--- index.js
+++ index.js
@@ -27,3 +27,28 @@ allParas.hide(1000) // it will hide all the paras after 1s creating a animation 
 
 $('#div1').text('makeme') // to change the innertext of the element with id div1
 // it will remove all the childs of this element
+
+
+// adding css with jquery
+
+$('#div2').css("width", 120 + 'px') 
+
+// you can even add multiple properties 
+
+$('#div2').css({
+    "width" : 50 + 'px',
+    "height" : 50 + 'px',
+    "backgroundColor" : "blue"
+})
+
+
+// you can add and remove class easily 
+
+$('#div3 div').addClass("disappear") // adds disappear class to all the div under div3
+
+$('div, p').addClass("makey") // adds makey class to both all divs and all p tags
+// you can also add two classes to a element
+
+// similar removeClass() and toggleClass() behaves 
+
+$("#div3 div").removeClass("disappear")
